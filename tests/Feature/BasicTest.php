452d1a1df44b030bcc04f23639ab528a2db37e16
Nilo Cara <nilo.cara@gmail.com>
2025-02-27 11:15:54 +0200

    System Reload

--- tests/Feature/BasicTest.php
+++ tests/Feature/BasicTest.php
@@ -9,10 +9,24 @@ class BasicTest extends TestCase
 {
     use RefreshDatabase;
 
+    /**
+     * Test basic application response.
+     *
+     * @return void
+     */
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
-
         $response->assertStatus(200);
+    }
+
+    /**
+     * Test database connection.
+     *
+     * @return void
+     */
+    public function test_database_connection(): void
+    {
+        $this->assertDatabaseCount('users', 0);
     }
 }
